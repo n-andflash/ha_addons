54dde02f435500c0753c854fb805f9ad867ccaa2
n-andflash <67410137+n-andflash@users.noreply.github.com>
2024-02-07 03:04:43 +0900

    실시간 에너지사용량의 소수점 자리수를 세팅할수 있도록 지원

--- sds_wallpad/sds_wallpad.py
+++ sds_wallpad/sds_wallpad.py
@@ -1067,7 +1067,7 @@ def serial_new_device(device, idn, packet):
                 payload["name"] = payload["name"].format(kor=kor)
                 payload["obj_id"] = payload["obj_id"].format(prefix=prefix, eng=eng)
                 payload["unit_of_meas"] = ("W", "m³/h", "m³/h")[idn]
-                payload["val_tpl"] = ("{{ value }}", "{{ value | float / 100 }}", "{{ value | float / 100 }}")[idn]
+                payload["val_tpl"] = "{{ value | float / {} }}".format(10 ** Options["rs485"]["{}_decimal".format(eng)])
                 if idn == 0:
                     payload["dev_cla"] = "power"
 
